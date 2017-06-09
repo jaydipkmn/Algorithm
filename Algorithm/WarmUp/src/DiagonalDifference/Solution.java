4a9871d493337f007f87eb333b188859a9ba4a66
Jaydip Kamani <jaydip.kamani@AMD.IBASET.COM>
2017-06-09 14:30:25 +0530

    Adding Algorithm solutions.

---
+++ Algorithm/WarmUp/src/DiagonalDifference/Solution.java
@@ -0,0 +1,23 @@
+package DiagonalDifference;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int leftD=0;
+        int rightD=0;
+        int size = in.nextInt();
+        int matrix[][] = new int[size][size];
+        for(int i=0, k=size-1; i< size; i++, k--){
+            for(int j=0; j< size; j++){
+                matrix[i][j] = in.nextInt();;            
+                if(i==j)
+                 leftD += matrix[i][j];                        
+            }
+            rightD += matrix[i][k];              
+        }
+
+        System.out.print(Math.abs(leftD-rightD));
+    }
+}
---
+++ Algorithm/WarmUp/src/SimpleArraySum/Solution.java
@@ -0,0 +1,20 @@
+package SimpleArraySum;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int sum =0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=in.nextInt();
+        }
+        System.out.println(sum);
+   }
+}
