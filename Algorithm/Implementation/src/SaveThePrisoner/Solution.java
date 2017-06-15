b91468f657f9644ad35178028f0b77e73b52488a
Jaydip Kamani <jaydipkmn@gmail.com>
2017-06-15 16:37:41 +0530

    Adding Save The Prisoner Solution to Implementation.

---
+++ Algorithm/Implementation/src/SaveThePrisoner/Solution.java
@@ -0,0 +1,18 @@
+package SaveThePrisoner;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int s = in.nextInt();
+            int result = (m-1+s-1)%n+1;
+            System.out.println(result);
+        }
+    }
+}
